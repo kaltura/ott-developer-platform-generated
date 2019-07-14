3cdcd2cb37fc606c7c081d2d1c3263f5992e4801
Tanya Libatter <tanya.libatter@kaltura.com>
2019-07-14 16:05:44 +0000

    Build site

--- polyfills.bundle.js
+++ polyfills.bundle.js
@@ -83,7 +83,7 @@ module.exports = {
   "specifications": {
     "ott": [{
       "openapi": "./ott.openapi.json",
-      "label": "5.2.4.18144"
+      "label": "5.2.5.17649"
     }]
   },
   "css": ["assets/ott.css"],
@@ -5258,7 +5258,7 @@ module.exports = {
   "openapi": {
     "ott": [{
       "openapi": "./ott.openapi.json",
-      "label": "5.2.4.18144"
+      "label": "5.2.5.17649"
     }]
   },
   "externalCSS": true
